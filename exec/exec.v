ca95d06ba66538d30f1288f44cf739f824f690f6
David Satime Wallin <david@dwall.in>
2021-05-05 21:32:55 +0200

    commented a lot of code and structs for future knowledge
    Signed-off-by: David Satime Wallin <david@dwall.in>

--- exec/exec.v
+++ exec/exec.v
@@ -6,28 +6,99 @@ import utils
 
 pub struct Cmd_object{
 	mut:
+	/*
+	cmd is the first given argument which
+	we will consider to be an application
+	or alias to be executed.
+	*/
 	cmd						string
+	/*
+	fullcmd is the joint string of the found
+	path leading to the application and the
+	first argument passed in (cmd).
+	*/
 	fullcmd					string
+	/* 
+	args will be all of the following args
+	sent [1..]. This will break if we find
+	a pipe sign |.
+	*/
 	args					[]string
+	/*
+	path is the first found path in paths
+	containing an executable with the same
+	name as cmd (first arg).
+	*/
 	path					string
+	/*
+	paths is what we declare in the config
+	-file (~/.vlshrc) when we set path=''.
+	
+	@todo: if we find multiple path='' then
+	these should be combined.
+	*/
 	paths					[]string
+	/*
+	aliases are all the alias= that we find
+	in the config -file (~/.vlshrc).
+	*/
 	aliases					map[string]string
+	/*
+	input is only used when handling pipes.
+	this is a placeholder for output captured
+	from the past command in the pipe chain.
+	*/
 	input					string
+	/*
+	set_redirect_stdio is only used when 
+	handling pipes. it's used in combination
+	with intercept_stdio to send output
+	along to next command as input.
+	*/
 	set_redirect_stdio		bool
+	/*
+	intercept_stdio is only used when handling
+	pipes. it's to know if we should slurp the
+	output from a command and set it as the 
+	input of the next command in the pipe chain.
+	*/
 	intercept_stdio			bool
+	/*
+	next_pipe_index is used to know which command
+	in the pipe chain to execute next. if -1
+	then it's the last command in the chain.
+	*/
 	next_pipe_index			int
 }
 
 pub struct Task {
 	mut:
+	/*
+	cmd is simply a command object
+	*/
 	cmd			Cmd_object
+	/*
+	pipe_string is only used when handling pipes.
+	it's the full string that we get from stdin
+	containing all commands and arguments
+	*/
 	pipe_string	string
+	/*
+	pipe_cmds is only used when handling pipes.
+	it'll be populated with a command object per
+	command found in the given pipe_string. the
+	next_pipe_index on a Cmd_object corresponds
+	to the indexes of this slice.
+	*/
 	pipe_cmds	[]Cmd_object
 }
 
 pub fn (mut t Task) prepare_task() ?string {
 	/*
-		prase possible pipes first.
+	parse pipe will normalize the pipe_string that
+	we get from stdin so that we remove unnecessary
+	whitespaces and possible double -pipes, etc.
+	then we populate our pipe_cmds -slice.
 	*/
 	t.parse_pipe()
 
@@ -94,15 +165,15 @@ fn (mut t Task) walk_pipes() {
 fn (mut t Task) exec() ?bool {
 
 	/*
-		checking if we have any aliases defined first that we should
-		overwrite any actual given command with.
+	checking if we have any aliases defined first that we should
+	overwrite any actual given command with.
 	*/
 	t.handle_aliases()
 
 	/*
-		locate the given command in specifide paths.
+	locate the given command in specifide paths.
 
-		@todo: should set some standard paths in the code..?
+	@todo: should set some standard paths in the code..?
 	*/
 	t.cmd.find_exe() or {
 
@@ -110,7 +181,7 @@ fn (mut t Task) exec() ?bool {
 	}
 
 	/*
-		also find_exec for each pipe cmd following.
+	also find_exec for each pipe cmd following.
 	*/
 	if t.pipe_cmds.len > 0 {
 		for i := 0; i < t.pipe_cmds.len; i++ {
@@ -122,12 +193,12 @@ fn (mut t Task) exec() ?bool {
 	}
 
 	/*
-		apply certain flags to specific commands if they aren't set manually.
+	apply certain flags to specific commands if they aren't set manually.
 	*/
 	t.cmd.internal_cmd_modifiers()
 
 	/*
-		also find internal_cmd_modifiers for each pipe cmd following.
+	also find internal_cmd_modifiers for each pipe cmd following.
 	*/
 	if t.pipe_cmds.len > 0 {
 		for i := 0; i < t.pipe_cmds.len; i++ {
@@ -135,10 +206,10 @@ fn (mut t Task) exec() ?bool {
 		}
 	}
 
-	//utils.debug('Task: $t')
-
 	/*
-		actually run the process.
+	actually run the process and check for a possible
+	next pipe cmd to run. an index of -1 will terminate
+	a pipe sequence.
 	*/
 	mut index := -1
 	index = t.run(t.cmd)
@@ -176,8 +247,8 @@ fn (mut t Task) run(c Cmd_object) (int) {
 	}
 	if c.intercept_stdio {
 		/*
-			set intercepted stdout for next
-			pipe cmd in chain.
+		set intercepted stdout for next
+		pipe cmd in chain.
 		*/
 		if c.next_pipe_index >= 0 {
 			output = child.stdout_slurp().trim_space()
@@ -228,6 +299,14 @@ fn (mut c Cmd_object) find_exe() ? {
 	return error('could not find and/or execute $trimmed_needle in $c.paths')
 }
 
+/*
+internal_cmd_modifiers is used to apply certain
+default flags on defined commands UNLESS we find that
+flag being set manually by the user.
+
+this function should be used a little as possible
+not to interfere with the users experience.
+*/
 fn (mut c Cmd_object) internal_cmd_modifiers() {
 	utils.debug('matching $c.cmd in built in modifiers')
 	match c.cmd {
